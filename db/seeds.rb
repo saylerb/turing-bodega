bd17e32a19dfa55f4ec4031454db96eb18c5e501
saylerb <sayler.b@gmail.com>
2016-06-05 11:26:22 -0600

    added a couple products to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+products = [ { name: "Sparkling Water", 
+               description: "All natural bubbly water",
+               image_path: "http://www.staples-3p.com/s7/is/image/Staples/s0896341_sc7?$splssku$" },
+             { name: "Breakfast Burrito",
+               description: "The best kind of burrito", 
+               image_path: "http://img14.deviantart.net/a9b6/i/2008/229/f/2/breakfast_burrito_by_cb_smizzle.jpg" }
+           ]
+
+
+products.each do |data|
+  product = Product.create(data)
+  puts "#{product.name} Successfully created"
+end
