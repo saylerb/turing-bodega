63c720a5de3729a5dcbb06ba97897064a9c28331
saylerb <sayler.b@gmail.com>
2016-06-05 15:05:03 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,10 @@ products = [ { name: "Sparkling Water",
                image_path: "http://www.staples-3p.com/s7/is/image/Staples/s0896341_sc7?$splssku$" },
              { name: "Breakfast Burrito",
                description: "The best kind of burrito", 
-               image_path: "http://img14.deviantart.net/a9b6/i/2008/229/f/2/breakfast_burrito_by_cb_smizzle.jpg" }
+               image_path: "http://img14.deviantart.net/a9b6/i/2008/229/f/2/breakfast_burrito_by_cb_smizzle.jpg" },
+             { name: "Paper Towel Dispenser",
+               description: "dont drip all the way down the hall, put this dispenser on the wall",
+               image_path: "http://i.imgur.com/j44csVm.jpg" }
            ]
 
 
