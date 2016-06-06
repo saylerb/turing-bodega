0cea42345b197f1e9ffb2b2e1ec8be11d27c471c
saylerb <sayler.b@gmail.com>
2016-06-05 22:27:57 -0600

    add more vote validation logic
    - add logic to controller to check to see if user logged in
    - add more information to product information page
      - add total votes counter
      - add list of people who voted

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,26 @@
 class VotesController < ApplicationController
+  before_action :require_login
+
   def create
-    # byebug
-    
     @vote = Vote.new(product_id: params[:product_id])
     @product = Product.find(params[:product_id])
     @vote.product = @product 
+    @vote.user = current_user
+
     if @vote.save
       flash[:notice] = "Successfully Voted for '#{@product.name}'"
+      redirect_to product_path(params[:product_id])
+    else
+      flash[:alert] = "Sorry, you've already voted"
+      redirect_to product_path(params[:product_id])
     end
-    redirect_to product_path(params[:product_id])
+  end
+
+  def require_login
+    unless current_user
+      flash[:alert] = "Sorry, you need to be logged in to do that"
+      redirect_to product_path(params[:product_id])
+    end
   end
 
 end
