d70b5f2c6c565e490a2886bd4599d46c53522491
saylerb <sayler.b@gmail.com>
2016-06-05 21:00:45 -0600

    add vote functionality
    - add vote spec and TDD the rest
    - generate vote model
    - migrate votes table
    - add relationships to Product model
    - add relationships to User model
    - add relationships to Vote model
    - add create logic to VotesController

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+
+  has_many :votes
+  has_many :users, through: :votes
+
+  def score
+    votes.count
+  end
 end
--- spec/features/user_votes_spec.rb
+++ spec/features/user_votes_spec.rb
@@ -1,19 +1,42 @@
 require "rails_helper"
 
 RSpec.feature "User votes" do 
+  scenario "on a product that exists" do
 
-  # As a user
+    # As a user
+    product = create_product
 
-  user = User.create(username: "brian", password: "password")
-  # When I visit a product page
-  
-  # I can click a vote button
-  # I see that I've voted on that thing (flash msg)
+    user = User.create(username: "brian", password: "password")
+    # When I visit a product page
 
-  # When I return to product index, it shows the total votes for that product
+    visit product_path(product)
+    
+    # I can click a vote button
+    click_link "Vote Up"
+    # I see a flash msg that I've voted on that thing (flash msg)
+
+    within "div#flash" do
+      expect(page).to have_content("Successfully Voted for '#{product.name}'")
+    end
+
+    # When I return to product index, it shows the total votes for that product
+
+    visit products_path
+
+    within "div#content" do
+      expect(page).to have_content("Sparkling Water")
+      expect(page).to have_content("Total Votes: 1")
+    end
+    
+
+    
+  end
 
   def create_product
-
+    data = { name: "Sparkling Water", 
+             description: "All natural bubbly water",
+             image_path: "http://www.staples-3p.com/s7/is/image/Staples/s0896341_sc7?$splssku$" }
+    Product.create(data)
   end
 
 end
